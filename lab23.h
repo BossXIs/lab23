234cfc60416468b64f79867183a690aa762c2c71
BossXI <bossweerawut@hotmail.com>
2022-02-08 22:26:53 +0700

    lab23.h

--- lab23.h
+++ lab23.h
@@ -10,11 +10,21 @@ using namespace std;
 class Equipment{
 	int hpmax;
 	int atk;
-	int def;
+	int def; 
 	public:
 		Equipment(int,int,int);
 		vector<int> getStat();			
 };
+
+Equipment::Equipment(int hp_eq, int atk_eq, int def_eq){
+	hpmax = hp_eq; atk = atk_eq; def = def_eq;
+}
+
+vector<int> Equipment::getStat(){
+	vector<int> Stat;
+	Stat.push_back(hpmax); Stat.push_back(atk); Stat.push_back(def);
+	return Stat;
+}
 
 class Unit{
 		string name;
@@ -54,6 +64,7 @@ Unit::Unit(string t,string n){
 	}
 	hp = hpmax;	
 	guard_on = false;
+	dodge_on = false;
 	equipment = NULL;
 }
 
@@ -74,14 +85,25 @@ void Unit::showStatus(){
 
 void Unit::newTurn(){
 	guard_on = false; 
+	dodge_on = false;
+	equipment = NULL;
 }
 
 int Unit::beAttacked(int oppatk){
 	int dmg;
+	int d = rand()%2;
 	if(oppatk > def){
 		dmg = oppatk-def;	
 		if(guard_on) dmg = dmg/3;
-	}	
+	}
+	if(dodge_on && d == 0){
+ 	 dmg = 0;
+ 	}else if(dodge_on && d == 1){
+ 	dmg = dmg*2;
+	}
+	 
+
+
 	hp -= dmg;
 	if(hp <= 0){hp = 0;}
 	
@@ -107,6 +129,40 @@ bool Unit::isDead(){
 	if(hp <= 0) return true;
 	else return false;
 }
+
+int Unit::ultimateAttack(Unit &opp){
+	return opp.beAttacked(atk*2);
+}
+ 
+
+
+void Unit::dodge(){
+	dodge_on = true;
+	beAttacked(atk);
+}
+
+
+void Unit::equip(Equipment *eq){
+	if (equipment){
+		if(eq->getStat() != equipment->getStat()){
+			hpmax -= (equipment->getStat()).at(0);
+			atk -= (equipment->getStat()).at(1);
+			def -= (equipment->getStat()).at(2);
+			equipment = eq;
+			hpmax += (equipment->getStat()).at(0);
+			atk += (equipment->getStat()).at(1);
+			def += (equipment->getStat()).at(2);
+		}
+	}
+	else{
+		equipment = eq;
+		hpmax += (equipment->getStat()).at(0);
+		atk += (equipment->getStat()).at(1);
+		def += (equipment->getStat()).at(2);
+	}
+	if(hp > hpmax) hp = hpmax;
+}
+
 
 void drawScene(char p_action,int p,char m_action,int m){
 	cout << "                                                       \n";
@@ -167,4 +223,6 @@ void playerLose(){
 	cout << "*                                                     *\n";
 	cout << "*******************************************************\n";
 };
+
+
 
